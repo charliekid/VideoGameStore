019b99ca72b2f3b2c165f38c1a34f3029f3cce21
Cathy Hsu <chsucathy@gmail.com>
2020-09-30 16:16:11 -0700

    Add partial files and pull request template (#1)
    * Add header partial
    
    * Add css styling to nav bar
    
    * Add footer partial
    
    * Create pull_request_template.md
    
    * Add citation text file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql');
+var hbs = require('express-handlebars');
 
 // Routers
 var indexRouter = require('./routes/index');
@@ -29,16 +30,17 @@ db.connect((err) => {
 });
 global.db = db;
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.engine('hbs', hbs({extname : 'hbs', defaultLayout: 'layout', layoutsDir: __dirname+'/views/'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

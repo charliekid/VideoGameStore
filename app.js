26704ae299eb344042258b669a66ac74c7601420
Cathy Hsu <chsucathy@gmail.com>
2020-10-07 16:46:45 -0700

    Create product page template (#6)
    * Add card template
    
    * Populate product page with example video games
    
    * Add citations
    
    * Clean up comments

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var logger = require('morgan');
 const mysql = require('mysql');
 var hbs = require('express-handlebars');
 
+
+
 // Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var databaseRouter = require('./routes/database');
 var signupRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
+var gamesRouter = require('./routes/games');
 var app = express();
 
 // create connection to database
@@ -34,7 +37,15 @@ global.db = db;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs', hbs({extname : 'hbs', defaultLayout: 'layout', layoutsDir: __dirname+'/views/'}));
+
+app.engine('hbs', hbs({extname : 'hbs', defaultLayout: 'layout', layoutsDir: __dirname+'/views/', helpers: {
+        sliceDescription: function(description) {
+            if (description.length > 100) {
+                return description.slice(0, 100) + "...";
+            }
+            return description;
+        }
+    }}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -48,6 +59,9 @@ app.use('/users', usersRouter);
 app.use('/database', databaseRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
+app.use('/games', gamesRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

69ae52db34b2ec12024fa89f1f73eb421bd84f3b
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-10-10 21:45:10 -0700

    got cart to populate with users data

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('cart', { title: 'Cart Stuff' });
+    // TODO: needs to get the user name from login somehow
+    let username = 'Dagger1';
+    let query = "SELECT * FROM cart_table WHERE username=" + "\'" + username + "\';"
+
+    db.query(query, (err, result) => {
+        if (err) {
+            console.log('error with the query');
+            res.render('views/error');
+        }
+        res.render('cart', {
+            title: 'Cart Stuff',
+            result: result
+        });
+    });
+
 });
 
 module.exports = router;

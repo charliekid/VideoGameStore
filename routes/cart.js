5972c395bbffe6a4e839430e1aa348083ec7f1a4
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-10-11 00:20:29 -0700

    got cart to update total

--- routes/cart.js
+++ routes/cart.js
@@ -6,18 +6,46 @@ router.get('/', function(req, res, next) {
     // TODO: needs to get the user name from login somehow
     let username = 'Dagger1';
     let query = "SELECT * FROM cart_table WHERE username=" + "\'" + username + "\';"
+    let sumQuery = "SELECT SUM(amount) AS cartSum from cart_table WHERE username=" + "\'" + username + "\';"
 
+    // I know this is confusing.. This queries for the user name
     db.query(query, (err, result) => {
         if (err) {
             console.log('error with the query');
             res.render('views/error');
         }
-        res.render('cart', {
-            title: 'Cart Stuff',
-            result: result
-        });
-    });
+        // This queries for a cart sum
+        db.query(sumQuery, (err, sumResult) => {
+            if (err) {
+                console.log('error with the query');
+                res.render('views/error');
+            }
+            console.log(sumResult);
+            console.log(result);
+            res.render('cart', {
+                title: 'Cart Stuff',
+                result: result,
+                sumResult: sumResult
+            });
+        }); // end of sum Query
+    }); // end of result query
 
+});
+
+router.post('/total', function(req, res, next) {
+    let username = 'Dagger1';
+    let sumQuery = "SELECT SUM(amount) from cart_table WHERE username=" + "\'" + username + "\';"
+    db.query(sumQuery, function(error, results, fields) {
+        if (error) {
+            console.log('error with the query');
+            res.render('views/error');
+        }
+
+        console.log(results);
+        res.json({
+            sumResult: results
+        });
+    }); // end of sum Query
 });
 
 module.exports = router;

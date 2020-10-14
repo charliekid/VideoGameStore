21f39897b179e5ab745a9fe97e8855e4dbf7d8c3
Cathy Hsu <chsucathy@gmail.com>
2020-10-14 14:05:52 -0700

    Create search bar

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Game Stuff' });
+router.get('/', async function(req, res, next) {
+  let r;
+  let result = await getCategories();
+  let categories = []
+  for (r of result) {
+    categories.push(r.rating);
+  }
+  res.render('index', { title: 'Game Stuff' , categories: categories});
 });
 
+function getCategories() {
+  let stmt = "SELECT DISTINCT rating FROM game_table ORDER BY rating";
+  return new Promise(function(resolve, reject) {
+    db.query(stmt, function(error, result) {
+      if (error) throw error;
+      // console.log(result);
+      resolve(result);
+      reject("Error");
+    });
+  });
+}
 module.exports = router;

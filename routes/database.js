f94915a804d3aec0c23db4e7ae74c7df61643f5f
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-10-15 15:16:22 -0700

    persistent login and logout

--- routes/database.js
+++ routes/database.js
@@ -7,7 +7,12 @@ router.get('/', function(req, res, next) {
     // the query we need to ask our database
     let query = 'SELECT * FROM user_table'
     // make a db connection and query
-    db.query
+    let loggedIn = false;
+
+    console.log("Session username in database ", req.session.username);
+    if(req.session.username) {// checks if this is null
+        let loggedIn = true;
+    }
     db.query(query, (err, result) => {
         if (err) {
             console.log('error with the query');
@@ -18,7 +23,6 @@ router.get('/', function(req, res, next) {
             information: result
         });
     });
-
 });
 
 module.exports = router;

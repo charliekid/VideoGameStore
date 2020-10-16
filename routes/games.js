d5fae1c8f38247d2af86f6ffe45c76fdb64a8cb6
Cathy Hsu <chsucathy@gmail.com>
2020-10-15 21:11:02 -0700

    Create search engine (#12)
    * Create search bar
    
    * Implement search functionality
    
    * Display no result found if search query does not return any games
    
    * Center search bars in the middle of page
    
    * Change default rating option to be "All"
    
    * Center no result found message
    
    * Change dropmenu menu option from All to All Ratings
    
    * Change search bar title from title to keyword

--- routes/games.js
+++ routes/games.js
@@ -2,15 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    let query = 'SELECT * FROM game_table'
+    let query = getSearchQuery(req.query.keyword, req.query.rating);
     // make a db connection and query
     db.query(query, (err, result) => {
         if (err) {
             console.log('error with the query');
             res.render('views/error');
         }
-        res.render('games', {games: result});
+        if (result.length != 0) {
+            res.render('games', {games: result});
+        }
+        else {
+            res.render('games', {noGames: true})
+        }
     });
 });
 
+function getSearchQuery(keyword, rating) {
+    let stmt;
+    if (!keyword && rating === "All") {
+        stmt = `SELECT * FROM game_table;`
+    } else if (keyword && !rating) {
+        stmt = `SELECT * FROM game_table WHERE gameTitle LIKE '%${keyword}%';`;
+    } else if (rating && !keyword) {
+        stmt = `SELECT * FROM game_table WHERE rating = '${rating}';`;
+    } else {
+        stmt = `SELECT * FROM game_table WHERE gameTitle LIKE '%${keyword}%' AND rating = '${rating}';`
+    }
+    return stmt;
+}
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Game Stuff' });
+router.get('/', async function(req, res, next) {
+  let r;
+  let result = await getRatings();
+  let ratings = []
+  for (r of result) {
+    ratings.push(r.rating);
+  }
+  res.render('index', { title: 'Game Stuff' , ratings: ratings});
 });
 
+function getRatings() {
+  let stmt = "SELECT DISTINCT rating FROM game_table ORDER BY rating";
+  return new Promise(function(resolve, reject) {
+    db.query(stmt, function(error, result) {
+      if (error) throw error;
+      // console.log(result);
+      resolve(result);
+      reject("Error");
+    });
+  });
+}
 module.exports = router;
